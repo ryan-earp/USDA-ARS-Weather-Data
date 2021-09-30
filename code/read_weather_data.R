3edf512e9381ba8e264c2d38611c3a23990910a3
Phillip D. Alderman <phillip.alderman@okstate.edu>
2021-09-30 10:02:18 -0500

    Adding code we developed in class previously

---
+++ code/read_weather_data.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(lubridate)
+library(readxl)
+
+
+year <- read_excel("../data/Weather_1940-01.xlsx",
+                   range = "D3:D3", 
+                   col_names = FALSE) %>% 
+  pull(...1)
+
+month <- read_excel("../data/Weather_1940-01.xlsx",
+                    range = "B3:B3", 
+                    col_names = FALSE) %>% 
+  pull(...1)
+
+
+cnames <- c("day", "Tmax", "Tmin", "Trange")
+
+wdata <- read_excel("../data/Weather_1940-01.xlsx",
+                    range = "A30:D60", 
+                    col_names = cnames) %>% 
+  mutate(year = year,
+         month = month)
+
+wdata %>% 
+  ggplot(aes(x = day, y = Tmax))+
+  geom_line()+
+  geom_line(aes(y = Tmin), color = "orange")
+
+wdata %>% 
+  filter(Tmax - Tmin != Trange)
