65572843930f3840c668065edbbbd02b416b58cb
Katherine Haile <katherine.haile@okstate.edu>
2021-11-09 10:26:17 -0600

    KH updata to add function

--- code/read_weather_data.R
+++ code/read_weather_data.R
@@ -149,8 +149,62 @@ wth_time <- date_loc(data = wth_data)
 
 # Full Function
 
+wth_an
+wth_import <- read_xlsx("data/raw/Weather_1940-01.xlsx",
+                        col_names = FALSE,
+                        na = c("", "-"))
+
+## Subset data section only and replace column names with correct names
+
+wth_data <- wth_import [c(30:60), c(1:14)] 
+
+headers <- c("day", "temp_maximum", "temp_minimum", "temp_range", "temp_set_max", 
+             "precip_time_of_beginning", "precip_time_of_ending", "precip_amount",
+             "precip_snowfall_in_inches", "precip_snow_depth_tobs",
+             "wind_dir_tobs", "weather_state_tobs", "wind_dir_day", "weather_state_day")
+
+names(wth_data) <- headers
+
+## Create month/year variables and site specific information
+
+month <- wth_import$...2[3]
+year <- wth_import$...4[3]
+location <- wth_import$...6[3]
+county <- wth_import$...8[3]
+state <- wth_import$...2[4]
+lat <- wth_import$...4[4]
+long <- wth_import$...6[4]
+
+## Create new data set with full date and site specific info
+
+wth_data_time <- wth_data %>% 
+  mutate(date = mdy(paste(month, day, year)), 
+         precip_time_of_beginning = format(times(as.numeric(precip_time_of_beginning))),
+         precip_time_of_ending = ifelse(is.na(as.numeric(precip_time_of_ending)),
+                                        precip_time_of_ending,
+                                        format(times(as.numeric(precip_time_of_ending)))),
+         location = rep(location, nrow(.)),
+         county = rep(county, nrow(.)),
+         state = rep(state, nrow(.)),
+         lat = rep(lat, nrow(.)),
+         long= rep(long, nrow(.))) %>%
+  subset(select = -c(day))
+
+## Possibilities to coerce precip begin and end times to include character values when needed
+
+precip_time_of_ending = (if(is.numeric(precip_time_of_ending)){
+  precip_time_of_ending = format(times(as.numeric(precip_time_of_ending)))
+} else {precip_time_of_ending = precip_time_of_ending})
+
+wth_data_time <- wth_data %>% 
+  mutate(precip_time_of_ending = ifelse(is.numeric(precip_time_of_ending),
+                                        precip_time_of_ending = format(times(as.numeric(precip_time_of_ending))),
+                                        precip_time_of_ending = precip_time_of_ending))  
 
 
+## Reorder columns into more logical format
+wth_data_time %>% 
+  select(date, location, county, state, lat, long, everything())
 
 
 
