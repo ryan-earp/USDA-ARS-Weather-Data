0df69c4e0218d8880f3fc2d5727260235bbb48d6
Phillip D. Alderman <phillip.alderman@okstate.edu>
2021-11-18 10:01:44 -0600

    Cleaning up run_script.R

--- code/run_script.R
+++ code/run_script.R
@@ -12,11 +12,5 @@ source("code/read_weather_data.R")
  wth_data <- wth_data %>% 
    unnest(imported_data)
  
- wth_data %>% 
-    mutate(wdt_type = map_chr(imported_data, ~class(.$wind_dir_tobs))) %>% 
-    filter()
- 
- wth_data %>% 
-   filter(map_lgl(imported_data, ~{"try-error" %in% class(.)})) %>% 
-   pull(imported_data)
+ write_csv(wth_data, "data/processed/imported_weather_data_1893-1940.csv")
  
