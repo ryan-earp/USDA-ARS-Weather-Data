205c09325a3d5abc6be523581b2f1b1c627500ec
Katherine Haile <katherine.haile@okstate.edu>
2021-12-08 10:31:15 -0600

    KH

--- code/clean_full_dataset.R
+++ code/clean_full_dataset.R
@@ -11,13 +11,63 @@ library(tidyverse)
 # Read file 
 wth_data <- read_csv("data/processed\\imported_weather_data_1893-1940.csv")
 
-wth_data %>% 
-  select(wind_dir_day) %>% 
-  summary()
 
-  pull(wind_dir_day) %>%
+# Precipitation Cleanup
+
+wth_data_precip_dif <- wth_data %>% 
+  select(date, precip_amount, precip_amount_orig) %>% 
+  filter(precip_amount != precip_amount_orig)
+
+## High values of rainfall
+wth_data %>% 
+  ggplot()+
+  geom_line(aes(x = date, y = precip_amount)) 
+
+
+precip <- wth_data %>% 
+  select(date, precip_amount) %>% 
+  drop_na() %>% 
+  arrange(desc(precip_amount))
+## Four values are large amounts that probably are not right
+
+
+# Character of day
+bad_states <- wth_data %>% 
+  select(weather_state_day) %>%
+  unique() 
+
+wth_data %>% 
+  group_by(weather_state_day) %>% 
+  ggplot(aes(x = weather_state_day)) +
+  geom_bar()
+
+## 49 different variables, most of them are misspellings but some are extra variables
+  
+  
+  
+# Wind Direction for day
+wind_dir <- 
+  wth_data %>% 
+  select(date, wind_dir_day) %>% 
+  filter(wind_dir_day == "?")
+
   unique()
 
+  pull(wind_dir_day) %>%
+  
+
+    
+    wind_dir <- 
+    wth_data %>% 
+    select(date, wind_dir_tobs) %>% 
+    filter(wind_dir_tobs == "SN")    
+    
+    
+str_replace_all(wind_dir, c(" //$"))  
+  #in read weather data file, make a function and add it to the original functions
+    
+  
+# Class Examples
 wth_data %>% 
   mutate(trange_diff = temp_maximum-temp_range) %>% 
   filter(trange_diff !=0) %>% 
@@ -31,33 +81,11 @@ coerce_sky <- function(condistion){
   cond_list
   
 }
-
-wth_data_precip <- wth_data %>% 
-  select(date, precip_amount, precip_amount_orig) %>% 
-  filter(precip_amount != precip_amount_orig)
-  
-wth_data_precip %>% 
-  ggplot()+
-  geom_line(aes(x = date, y = precip_amount)) +
-  geom_line(aes(x = date, y = precip_amount_orig))
-
-drop_na()
          
-         
-wth_data %>% 
-  select()
 
-head(wth_data)
-
-
-precip_amount_orig
 
 wth_data %>% 
-  ggplot(aes(x = date, y = precip_amount))+
-  geom_line()
-
-
-
+  colnames()
 
   
   
